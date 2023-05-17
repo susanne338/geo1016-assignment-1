5f2617231d184c5c508b19e2167a999b4c40e226
putiriyadi <116630814+putiriyadi@users.noreply.github.com>
2023-05-17 15:11:52 +0200

    quantitative error calculation
    compare RMSE value of set of points

---
+++ main.py
@@ -0,0 +1,46 @@
+# rmse calculation
+import numpy as np
+import math
+
+# change the path!
+inputcfile = "/Users/putiriyadi/Documents local/TU/q4/photogrammetry/A1_Calibration/errorcalc/test_0_12pts.txt"
+outputcfile = "/Users/putiriyadi/Documents local/TU/q4/photogrammetry/A1_Calibration/errorcalc/test_0_12pts_output.txt"
+
+# read input coordinate file
+f = open(inputcfile,"r")
+inputlines = f.readlines()
+# horizontal line along x, vertical along y axis
+input_coord_h = []
+input_coord_v = []
+for x in inputlines:
+    input_coord_h.append(int(x.split(' ')[3]))
+    input_coord_v.append(int(x.split(' ')[4]))
+f.close()
+
+# read output coordinate file
+f = open(outputcfile,"r")
+outputlines = f.readlines()
+# horizontal line along x, vertical along y axis
+output_coord_h = []
+output_coord_v = []
+for y in outputlines:
+    output_coord_h.append(int(y.split(' ')[3]))
+    output_coord_v.append(int(y.split(' ')[4]))
+f.close()
+
+print("for ", len(input_coord_h), " correspondent points.")
+print("horizontal:")
+print(input_coord_h)
+print(output_coord_h)
+h_mse = np.square(np.subtract(input_coord_h,output_coord_h)).mean()
+h_rmse = math.sqrt(h_mse)
+
+v_mse = np.square(np.subtract(input_coord_v,output_coord_v)).mean()
+v_rmse = math.sqrt(v_mse)
+
+print("horizontal rmse: ", h_rmse)
+
+print("vertical:")
+print(input_coord_v)
+print(output_coord_v)
+print("vertical rmse: ", v_rmse)
