1afefafeaedea9008cb5f4cc9b5047060ba22e06
putiriyadi <116630814+putiriyadi@users.noreply.github.com>
2023-05-18 21:55:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -18,29 +18,40 @@ for x in inputlines:
 f.close()
 
 # read output coordinate file
-f = open(outputcfile,"r")
-outputlines = f.readlines()
+of = open(outputcfile,"r")
+outputlines = of.readlines()
 # horizontal line along x, vertical along y axis
 output_coord_h = []
 output_coord_v = []
 for y in outputlines:
     output_coord_h.append(int(y.split(' ')[3]))
     output_coord_v.append(int(y.split(' ')[4]))
-f.close()
+of.close()
+
+h_diff = np.subtract(input_coord_h,output_coord_h)
+v_diff = np.subtract(input_coord_v,output_coord_v)
+dist_list = []
+
+for i, j in zip(h_diff, v_diff):
+    dist = math.sqrt(i*i + j*j)
+    dist_list.append(dist)
+
+dist_rmse = math.sqrt(np.square(dist_list).mean())
+
+h_mse = np.square(np.subtract(input_coord_h,output_coord_h)).mean()
+h_rmse = math.sqrt(h_mse)
+v_mse = np.square(np.subtract(input_coord_v,output_coord_v)).mean()
+v_rmse = math.sqrt(v_mse)
 
 print("for ", len(input_coord_h), " correspondent points.")
 print("horizontal:")
 print(input_coord_h)
 print(output_coord_h)
-h_mse = np.square(np.subtract(input_coord_h,output_coord_h)).mean()
-h_rmse = math.sqrt(h_mse)
-
-v_mse = np.square(np.subtract(input_coord_v,output_coord_v)).mean()
-v_rmse = math.sqrt(v_mse)
-
 print("horizontal rmse: ", h_rmse)
 
 print("vertical:")
 print(input_coord_v)
 print(output_coord_v)
 print("vertical rmse: ", v_rmse)
+
+print("distance rmse: ", dist_rmse)
